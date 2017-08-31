9c5efca1a6fb696b4f31c6227e0c8c88001bde4d
DaOnlyOwner <jan@hemming-haas.de>
2017-08-31 13:00:08 +0200

    Better algorithm.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <string>
-#include <iostream>
 #include <fstream>
 #include <array>
 #include <vector>
@@ -55,6 +53,16 @@ public:
         }
     }
 
+    SudokuField GetSolvedSudoku()
+    {
+        return m_field;
+    }
+
+    SudokuField& GetSolvedSudokuByRef()
+    {
+        return m_field;
+    }
+
     void Print()
     {
         std::cout << "-------------------------";
@@ -86,17 +94,24 @@ public:
     }
 
 private:
-    std::tuple<int, int> find_empty_cell(int xStart, int yStart, const SudokuField &field)
+    std::tuple<int, int> find_empty_cell(int xStart, int yStart, const SudokuField &field, bool exclusive = false)
     {
+        if (exclusive)
+        {
+            xStart = (xStart + 1) % 9;
+            yStart = xStart == 0 ? yStart + 1 : yStart;
+            if (yStart > 8) return {-1, -1}; // Last cell
+        }
+
         for (int y = yStart; y < 9; y++)
         {
             for (int x = xStart; x < 9; x++)
             {
-                if (field[y][x].number == 0) return {x,y};
+                if (field[y][x].number == 0) return {x, y};
             }
             xStart = 0;
         }
-        return {-1,-1};
+        return {-1, -1};
     };
 
     void gen_possible_number(const SudokuField &array, int x, int y, std::vector<int> &possible_correct_number)
@@ -155,42 +170,83 @@ private:
     }
 
     /**
-     * @long Explanation:
-     * The algorithm starts with the first empty cell it encounters.
-     * It tries to find a possible correct number.
-     * Case 1: Found:
-     * Based on this information it moves onto the next field and repeats the number finding recursively.
-     * If no possible correct number can be found it returns and repeats with the next possible correct number.
+     * @brief The algorithm finds an empty cell and assigns a possible correct number to it. If no possible number can be found: Backtrack, if no cell can be found: Return true.
+     * @long
+     * 1. Check if c is a valid cell.
+     *    Case 1: It's not
+     *            Return the value of a validity check.
+     * 2. Find the next empty cell c' = (x',y') starting from (x,y) so that (x,y) != (x',y')
+     * 3. Foreach possible correct number n for cell c at (x,y):
+     *      3.1. Set the value of c to n
+     *      3.3. Call solve_sudoku(x',y', reference to current sudokufield)
+     *           Case 1: False is returned:
+     *                   Then continue looping.
+     *           Case 2: True is returned:
+     *                   Then return True exiting this function early.
+     * 4. Set the value of c to 0 and return False.
+     *
+     * @Correctness
+     *
+     * Sudokufield' : The state of the Sudokufield that it had when it was passed in (Think of a local copy).
+     *
+     * I: If solve_sudoku(int x,int y,SudokuField& f) returns True then f is in a valid state and completed.
+     *    If it returns False then f' has errors; f = f' in this case.
+     *
+     *    In 1. Case 1: Returning True implies that the f is completely and correctly filled in with numbers.
+     *                  Completness: x and y are values that are previously returned by finding an empty cell. If there are no empty cells then x and y are in an invalid state. (Has to be implemented that way) (Fullfills I)
+     *                  Correctness: Is guaranteed after a validity check (Fullfills I)
+     *                  Returning False implies the same, but this time the Sudoku Field has an invalid configuration. (Fullfills I)
+     *
+     *    In 3. Case 1: By Induction we can assume that I holds for a call to solve_sudoku(x', y', reference to f, that was modified in 3.1).
+     *                  - If True (Case 2.) is returned by this call then n was a correct choice for f (by Induction) and we return True indicating that f has no errors. Also f is completed (by Induction) which fullfills I.
+     *                  - If False (Case 1.) is returned then there is a number in our current configuration that puts f' into an invalid state.
+     *                    -> Assume n is the cause. We thus have to find a valid number by iterating over the rest.
+     *                          -> None of them is valid. End up in 4., delete our choice to set f back to the state it was in previously (Requirement of I)(set the value of c to 0) and return False because the error was introduced earlier. Fullfills I.
+     *                          -> One of them is valid. End up in Case 2.
+     *                    -> Assume n is not the cause and would be a correct choice. Then looping will do no harm. solve_sudoku() will always return False in this case because the invalid number was introduced earlier somewhere in f'. So we end up in 4., delete our choice, so that f = f' and return False, fullfilling I.
+     *
+     *    Calling solve_sudoku(0,0,f) will thus return True and F is solved, or False and F cannot be solved.
      *
      * @param in - a sudoku field
      * @param out - the solved sudoku field
      */
-    bool solve_sudoku_inner(int x, int y, const SudokuField &in)
+    bool solve_sudoku_inner(int x, int y, SudokuField &in_out)
     {
+
+        if (x == -1 || y == -1) return is_valid(in_out); // 1.
+        int xNew, yNew;
+        std::tie(xNew, yNew) = find_empty_cell(x,y, in_out, true); // 2.
+
         std::vector<int> possible_nums;
-        gen_possible_number(in,x,y,possible_nums);
+        gen_possible_number(in_out,x,y,possible_nums);
 
-        if (possible_nums.size() != 0)
+        for(int num : possible_nums) // 3.
         {
-            SudokuField new_field ( in );
-            for(int num : possible_nums)
-            {
-                new_field[y][x].number = num;
-
-                int xNew, yNew;
-                std::tie(xNew,yNew) = find_empty_cell(x,y,new_field);
-
-                if (xNew == -1 || yNew == -1) { m_field = new_field; return true;} // Okay, cannot find empty cells anymore. Everything correctly inserted now.
-                if (solve_sudoku_inner(xNew,yNew,new_field)) {return true;} // Just unwind, m_field is set already.
-            }
+            in_out[y][x].number = num; // 3.1
+            if (solve_sudoku_inner(xNew,yNew,in_out)) return true; // 3.2
+            // 3.1 needs no additional check.
         }
-        return false; // Backtrack
+
+        in_out[y][x].number = 0; // 4.
+        return false; // 4.
+
     }
 
     // TODO: Implement
-    bool CheckValidity()
+    bool is_valid(const SudokuField& in)
     {
+        for(int y = 0; y<9; y++)
+        {
+            for(int x = 0; x<9; x++)
+            {
+                const Cell& cell = in[y][x];
+                std::vector<int> possible_nums;
+                gen_possible_number(in,x,y,possible_nums);
+                if (possible_nums.empty()) return false;
+            }
+        }
 
+        return true;
     }
 
 
